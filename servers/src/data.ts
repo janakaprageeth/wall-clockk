da2a4559c347dcf27e4510569d2c719dcc129eb6
janakaprageeth <janakaprageeth99@gmail.com>
2023-08-14 15:11:42 +0530

    update

---
+++ servers/src/data.ts
@@ -0,0 +1,119 @@
+
+
+export const WATCHES: any[] = [
+  {
+    id:'1',
+    name: 'Zigtag',
+    price: 350,
+    favorite: false,
+    origins: ['italy'],
+    imageUrl: 'assets/11.png',
+    tags: ['Zig', 'Hot', 'Cool'],
+  },
+  {
+    id:'2',
+    name: 'Classico',
+    price: 300,
+    favorite: true,
+    origins: ['persia', 'middle east', 'china'],
+    imageUrl: 'assets/12.png',
+    tags: ['Zig', 'Classic'],
+  },
+  {
+    id:'3',
+    name: 'Zigtag-Classico',
+    price: 400,
+    favorite: false,
+    origins: ['germany', 'us'],
+    imageUrl: 'assets/13.png',
+    tags: ['Zig', 'Golden'],
+  },
+  {
+    id:'4',
+    name: 'Women face clock',
+    price: 320,
+    favorite: true,
+    origins: ['belgium', 'france'],
+    imageUrl: 'assets/14.png',
+    tags: ['Zig', 'Hot'],
+  },
+  {
+    id:'5',
+    name: 'BRaun',
+    price: 300,
+    favorite: false,
+    origins: ['india', 'asia'],
+    imageUrl: 'assets/15.png',
+    tags: ['Zig', 'Cool'],
+  },
+  {
+    id:'6',
+    name: 'Wooden-classico',
+    price: 260,
+    favorite: false,
+    origins: ['india', 'asia'],
+    imageUrl: 'assets/16.png',
+    tags: ['Classic', 'Cool'],
+  },
+  {
+    id:'7',
+    name: 'Red-Leni',
+    price: 280,
+    favorite: true,
+    origins: ['US', 'asia'],
+    imageUrl: 'assets/17.png',
+    tags: ['Modern', 'Classic'],
+  },
+  {
+    id:'8',
+    name: 'Old-Classic',
+    price: 300,
+    favorite: false,
+    origins: ['UK', 'Canada'],
+    imageUrl: 'assets/18.png',
+    tags: ['Old', 'Classic'],
+  },
+  {
+    id:'9',
+    name: 'White-Titan',
+    price: 310,
+    favorite: true,
+    origins: ['AUS', 'asia'],
+    imageUrl: 'assets/19.png',
+    tags: ['Modern', 'Hot'],
+  },
+  {
+    id:'10',
+    name: 'Modern-Classico',
+    price: 3580,
+    favorite: false,
+    origins: ['LK', 'UK'],
+    imageUrl: 'assets/20.png',
+    tags: ['Modern', 'Classic'],
+  },
+]
+
+export const watch_tags:any[] = [
+  { name: 'All', count: 5 },
+  { name: 'Zig', count: 5 },
+  { name: 'Classic', count: 5 },
+]
+
+export const sample_users:any[] = [
+  {
+    name: 'Janaka',
+    email: 'janaka@gmail.com',
+    password: '123456',
+    address: '123 Main St',
+    isAdmin:true,
+
+  },
+  {
+    name: 'Jig',
+    email: 'jig@gmail.com',
+    password: '123456',
+    address: '123 Main St',
+    isAdmin:false,
+
+  }
+]
